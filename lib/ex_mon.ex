9b5591483100e18a19cf408f28a2697674bb2fed
GabsRocca <roccaesquil19910@gmail.com>
2023-11-01 01:28:01 -0300

    exmon_create_computer_player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias Exmon.Player
+  alias Exmon.{Game, Player}
+  @computer_name "Robotinick"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player("Robotinick", :punch, :kick, :heal)
+    |> computer = create_player(:punch, :kick, :heal)
+    Game.start(player)
   end
 end
