2193483a0257afe160e57b3bb8c1354e1265d53d
GabsRocca <roccaesquil19910@gmail.com>
2023-11-02 09:53:41 -0300

    teste_integ

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -4,6 +4,6 @@ defmodule ExMon.Game.Status do
     def print_round_message() do
         IO.puts ("\n======The Game is Started=======\n")
         IO.inspect(Game.info)
-        IO.puts("----------------------")
+        IO.puts("------------------------------")
     end
 end
