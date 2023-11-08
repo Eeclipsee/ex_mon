45e62dd443e79ebdaa0acf8030c35e38cf58d19c
GabsRocca <roccaesquil19910@gmail.com>
2023-11-08 14:25:18 -0300

    create_actions_player

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -3,7 +3,9 @@ defmodule ExMon.Game.Status do
     
     def print_round_message() do
         IO.puts ("\n======The Game is Started=======\n")
+        IO.puts ("---------------------------------")
         IO.inspect(Game.info)
-        IO.puts("------------------------------")
+        IO.puts ("---------------------------------")
+        IO.puts("=================================")
     end
 end
