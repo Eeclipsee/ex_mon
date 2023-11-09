93ecb4b6f5bf8929da6afe4d0af37a98d868f28f
GabsRocca <roccaesquil19910@gmail.com>
2023-11-09 02:03:02 -0300

    make_move_deduction_lifepoints(erro)

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -12,4 +12,13 @@ defmodule ExMon.Game.Status do
     defp print_wrong_move_message(move) do
         IO.puts("\n====== Invalid move: #{move}. ======\n")
     end
+
+    def print_move_message(:computer, :attack, damage) do
+        IO.puts("\n===== The Player attacked the computer dealing #{damage} damage. =====\n")
+    end
+
+    
+    def print_move_message(:player, :attack, damage) do
+        IO.puts("\n===== The Computer attacked the player dealing #{damage} damage. =====\n")
+    end
 end
