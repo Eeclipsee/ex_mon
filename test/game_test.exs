bb99dc400a2aeb1d5665a1cfe767d879671c1620
exmonprojeto <projeto_exmon@proton.me>
2023-11-17 10:14:39 -0300

    create_test_module_gametest

--- test/game_test.exs
+++ test/game_test.exs
@@ -1,8 +1,6 @@
 defmodule ExMon.GameTest do
  use ExUnit.Case
-
  alias ExMon.{Game, Player}
-
     describe "start/2" do
         test "Starts the game state" do
             player = Player.build("Gabriel", :chute, :soco, :cura)
@@ -11,6 +9,4 @@ defmodule ExMon.GameTest do
             assert {:ok, _pid} = Game.start(computer, player)
         end
     end
-
- 
 end
